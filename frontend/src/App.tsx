f5bd65c74a78341635e7fe9b9732ef49d002f644
chuotchuonchetra <tranevercare2504@gmail.com>
2026-04-29 09:36:50 +0700

    create front-end project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <p className='text-3xl font-bold underline text-amber-100'>Hello world!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
